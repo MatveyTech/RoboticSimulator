484199f5581517a350b304d55bf7f872fbb360cf
Matvey <matvey@mazorrobotics.com>
2018-10-30 14:34:59 +0200

    Parsing rbs file - in work - LNK2005

--- include/Utils.h
+++ include/Utils.h
@@ -1,221 +1,175 @@
 #pragma once
-//
-//#include <stdarg.h>
-//#include <string.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <vector>
-//#include "Logger.h"
-//
-//#include <MathLib/V3D.h>
-//#include <MathLib/Plane.h>
-//#include "Assert.h"
-//
-//#include <vector>
-//#include <string>
-//#include <fstream>
-//
-//#pragma warning( disable : 4996)
-//
-//#define DynamicArray std::vector
-//typedef unsigned int uint;
-//
-//
-//class Globals {
-//public:
-//	// gravitational acceleration 
-//	static double g;
-//	//this is the direction of the up-vector
-//	static V3D worldUp;
-//	//and the ground plane
-//	static Plane groundPlane;
-//	//this is the total ellapsed sim time
-//	static double currentSimulationTime;
-//};
-//
-//typedef struct key_word {
-//	char keyWord[50];
-//	int retVal;
-//}KeyWord;
-//
-//// given a list of keywords that map strings to integer values denoting types of keywords, this method will determine the type of command that is passed in
-//// 'line' will be modified after this function call to point to the first character after the keyword. If no keywords are matched, the method returns -1
-//int getLineType(char* &line, KeyWord* keywords, int nKeywords);
-//
-//// given a list of keywords that map strings to integer values denoting types of keywords, this method will determine the string corresponding to the token passed in
-//char* getKeyword(int lineType, KeyWord* keywords, int nKeywords);
-//
-///**
-//	This method throws an error with a specified text and arguments 
-//*/
-//inline void throwError(const char *fmt, ...){		// Custom error creation method
-//	char *pBuffer = NULL;
-//	GET_STRING_FROM_ARGUMENT_LIST(fmt, pBuffer);
-//
-//	Logger::print("Error Thrown: %s\n", pBuffer);
-//	
-//	throw pBuffer;
-//
-//	RELEASE_STRING_FROM_ARGUMENT_LIST(pBuffer);
-//}
-//
-////interpret the array of characters as a sequence of strings...
-//inline void getCharSeparatedStringList(const char* cString, std::vector<std::string>& lines, char separator = '\n') {
-//	lines.clear();
-//	std::string line;
-//	for (const char *c = cString;*c;c++) {
-//		if (*c && *c != separator)
-//			line.append(1, *c);
-//		else {
-//			lines.push_back(line);
-//			line.clear();
-//		}
-//	}
-//	if (line.size() > 0)
-//		lines.push_back(line);
-//}
-//
-///**
-//	This method reads all the doubles from the given file and stores them in the array of doubles that is passed in
-//*/
-//inline void readDoublesFromFile(FILE* f, DynamicArray<double> *d){
-//	double temp;
-//	char line[200];
-////	while (fscanf(f, "%lf\n", &temp) == 1)
-////		d->push_back(temp);
-//
-//	while (!feof(f)){
-//		if (fscanf(f, "%lf\n", &temp) == 1)
-//			d->push_back(temp);
-//		else
-//			fgets(line, sizeof(line)/sizeof(line[0])-1, f);
-//	}
-//}
-//
-///**
-//	This method returns a pointer to the first non-white space character location in the provided buffer
-//*/
-//inline char* lTrim(char* buffer){
-//	while (*buffer==' ' || *buffer=='\t' || *buffer=='\n' || *buffer=='\r')
-//		buffer++;
-//	return buffer;
-//}
-//
-//inline char* rTrim(char* buffer){
-//	int index = (int)strlen(buffer) - 1;
-//	while (index>=0){
-//		if (buffer[index]==' ' || buffer[index]=='\t' || buffer[index]=='\n' || buffer[index]=='\r'){
-//			buffer[index] = '\0';
-//			index--;
-//		}
-//		else
-//			break;
-//	}
-//	return buffer;
-//}
-//
-//inline char* trim(char* buffer){
-//	return rTrim(lTrim(buffer));
-//}
-//
-///**
-//	This method reads a line from a file. It does not return empty lines or ones that start with a pound key - those are assumed to be comments.
-//	This method returns true if a line is read, false otherwise (for instance the end of file is met).
-//*/
-//inline bool readValidLine(char* line, int nChars, FILE* fp){
-//	line[0] = '\0';
-//	while (!feof(fp)){
-//		fgets(line, nChars, fp);
-//		if ((int)strlen(line)>=nChars)
-//			throwError("The input file contains a line that is longer than the buffer - not allowed");
-//		char* tmp = trim(line);
-//		if (tmp[0]!='#' && tmp[0]!='\0')
-//			return true;
-//	}
-//
-//	return false;
-//}
-//
-//
-///**
-//	This method reads a line from a file. It does not return empty lines or ones that start with a pound key - those are assumed to be comments.
-//	This method returns true if a line is read, false otherwise (for instance the end of file is met).
-//*/
-//inline bool readValidLine(char* line, FILE* fp, int n){
-//	line[0] = '\0';
-//	while (!feof(fp)){
-//		fgets(line, n, fp);
-//		if ((int)strlen(line) > n-5)
-//			Logger::print("rearValidLine: Warning. The read file exceeds the buffer limits. The line was probably not read in its entirety.");
-//		char* tmp = trim(line);
-//		if (tmp[0]!='#' && tmp[0]!='\0')
-//			return true;
-//	}
-//
-//	return false;
-//}
-//
-//inline bool isWhiteSpace(char ch){
-//	return (ch==' ' || ch=='\t' || ch=='\n' || ch=='\r' || ch=='\0');
-//}
-//
-//
-///**
-//	This method returns a DynamicArray of char pointers that correspond to the addressed
-//	of the tokens that are separated by white space in the string that is passed in as a pointer.
-//*/
-//inline DynamicArray<char*> getTokens(char* input){
-//	DynamicArray<char*> result;
-//	input = lTrim(input);
-//	//read in the strings one by one - assume that each tokens are less than 100 chars in length
-//	while (input[0]!='\0'){
-//		result.push_back(input);
-//		char tempStr[100];
-//		sscanf(input, "%s", tempStr);
-//		input = lTrim(input + strlen(tempStr));
-//	}
-//	return result;
-//}
-//
-///**
-//	Checks extension (for loading files).
-//*/
-//bool StringExtensionHas(const char* _csName, const char* _ext);
-//
-///**
-//	Checks if pointer is null before deleting.
-//	Pass a reference to pointer (not a copy!)
-//*/
-//
-//template<class T>
-//void SafeDelete(T** _ppT)
-//{
-//	if (*_ppT)
-//	{
-//		delete *_ppT;
-//		*_ppT = NULL;
-//	}
-//		
-//}
-//
-//template<class T>
-//void SafeDeleteArray(T** _ppT)
-//{
-//	if (*_ppT)
-//	{
-//		delete[] *_ppT;
-//		*_ppT = NULL;
-//	}
-//}
-//
-//inline bool TestFileExists(const std::string& name) {
-//	std::ifstream f(name.c_str());
-//	return f.good();
-//}
-//
-//// *************************************************************************************************************************
-//std::istringstream readValidLineAsStream(std::ifstream& stream);
-//std::string readValidLine(std::ifstream& stream);
-//std::string boolToString(bool boolValue);
-//bool stringToBool(std::string boolString);
-//
+
+#include <fstream>
+#include <string>
+
+std::string readValidLine(std::ifstream& stream); 
+
+typedef struct key_word {
+	char keyWord[50];
+	int retVal;
+}KeyWord;
+
+/**
+This method returns a pointer to the first non-white space character location in the provided buffer
+*/
+inline char* lTrim(char* buffer) {
+	while (*buffer == ' ' || *buffer == '\t' || *buffer == '\n' || *buffer == '\r')
+		buffer++;
+	return buffer;
+}
+
+inline char* rTrim(char* buffer) {
+	int index = (int)strlen(buffer) - 1;
+	while (index >= 0) {
+		if (buffer[index] == ' ' || buffer[index] == '\t' || buffer[index] == '\n' || buffer[index] == '\r') {
+			buffer[index] = '\0';
+			index--;
+		}
+		else
+			break;
+	}
+	return buffer;
+}
+
+inline char* trim(char* buffer) {
+	return rTrim(lTrim(buffer));
+}
+
+inline bool isWhiteSpace(char ch) {
+	return (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || ch == '\0');
+}
+
+/**
+	This method reads a line from a file. It does not return empty lines or ones that start with a pound key - those are assumed to be comments.
+	This method returns true if a line is read, false otherwise (for instance the end of file is met).
+**/
+inline bool readValidLine(char* line, int nChars, FILE* fp){
+	line[0] = '\0';
+	while (!feof(fp)){
+		fgets(line, nChars, fp);
+		if ((int)strlen(line)>=nChars)
+			throw("The input file contains a line that is longer than the buffer - not allowed");
+		char* tmp = trim(line);
+		if (tmp[0]!='#' && tmp[0]!='\0')
+			return true;
+	}
+
+	return false;
+}
+// given a list of keywords that map strings to integer values denoting keyword types, this method will determine the type of command that is passed in
+int getLineType(char* &line, KeyWord* keywords, int nKeywords) {
+	for (int i = 0; i < nKeywords; i++) {
+		if (strncmp(line, keywords[i].keyWord, strlen(keywords[i].keyWord)) == 0 && isWhiteSpace(line[strlen(keywords[i].keyWord)])) {
+			line += strlen(keywords[i].keyWord);
+			return keywords[i].retVal;
+		}
+	}
+	return -1;
+}
+
+enum RB_KEYWORDS {
+	RB_NOT_IMPORTANT = -1,
+	RB_RB,
+	RB_END_RB,
+	RB_NAME,
+	RB_MASS,
+	RB_MOI,
+	RB_POSITION,
+	RB_ORIENTATION,
+	RB_VELOCITY,
+	RB_ANGULAR_VELOCITY,
+	RB_FRICTION_COEFF,
+	RB_RESTITUTION_COEFF,
+	RB_IS_FROZEN,
+	RB_CDP,
+	RB_MESH_NAME,
+	RB_MATERIAL,
+	RB_MATERIAL_DEFINITION,
+	RB_COLOR,
+	RB_CHILD,
+	RB_PARENT,
+	RB_PPOS,
+	RB_CPOS,
+	RB_HINGE_JOINT,
+	RB_UNIVERSAL_JOINT,
+	RB_BALL_AND_SOCKET_JOINT,
+	RB_WELDED_JOINT,
+	RB_JOINT_LIMITS,
+	RB_JOINT_ROT_AXES,
+	RB_JOINT_CONTROL_MODE,
+	RB_JOINT_END,
+	RB_END_EFFECTOR,
+	RB_BODY_POINT_FEATURE,
+	RB_THICKNESSS,
+	RB_MOTOR_ID,
+	RB_FLIPMOTORAXISDIR,
+	RB_MESH_TRANSFORMATION,
+	RB_MAPPING_INFO,
+	RB_MESH_DESCRIPTION,
+	RB_DEFAULT_ANGLE,
+	RB_COMPLIANT_JOINT,
+	RB_COMPLIANT_JOINT_STIFFNESS
+};
+
+KeyWord RBkeywords[] = {
+	{ "RigidBody", RB_RB },
+	{ "/End", RB_END_RB },
+	{ "name", RB_NAME },
+	{ "mass", RB_MASS },
+	{ "moi", RB_MOI },
+	{ "position", RB_POSITION },
+	{ "orientation", RB_ORIENTATION },
+	{ "velocity", RB_VELOCITY },
+	{ "angularVelocity", RB_ANGULAR_VELOCITY },
+	{ "frictionCoefficient", RB_FRICTION_COEFF },
+	{ "restitutionCoefficient", RB_RESTITUTION_COEFF },
+	{ "frozen", RB_IS_FROZEN },
+	{ "CDP", RB_CDP },
+	{ "mesh", RB_MESH_NAME },
+	{ "material", RB_MATERIAL },
+	{ "defineMaterial", RB_MATERIAL_DEFINITION },
+	{ "colour", RB_COLOR },
+	{ "child", RB_CHILD },
+	{ "parent", RB_PARENT },
+	{ "jointPPos", RB_PPOS },
+	{ "jointCPos", RB_CPOS },
+	{ "hingeJoint", RB_HINGE_JOINT },
+	{ "universalJoint", RB_UNIVERSAL_JOINT },
+	{ "ballAndSocketJoint", RB_BALL_AND_SOCKET_JOINT },
+	{ "weldedJoint", RB_WELDED_JOINT },
+	{ "jointLimits", RB_JOINT_LIMITS },
+	{ "jointAxes", RB_JOINT_ROT_AXES },
+	{ "controlMode", RB_JOINT_CONTROL_MODE },
+	{ "/Joint", RB_JOINT_END },
+	{ "endEffector", RB_END_EFFECTOR },
+	{ "bodyPointFeature", RB_BODY_POINT_FEATURE },
+	{ "thickness", RB_THICKNESSS },
+	{ "motorID", RB_MOTOR_ID },
+	{ "flipMotorAxis", RB_FLIPMOTORAXISDIR },
+	{ "meshTransformation", RB_MESH_TRANSFORMATION },
+	{ "mappingInfo", RB_MAPPING_INFO },
+	{ "meshDescription", RB_MESH_DESCRIPTION },
+	{ "defaultAngle", RB_DEFAULT_ANGLE },
+	{ "compliantJoint",RB_COMPLIANT_JOINT },
+	{ "stiffness", RB_COMPLIANT_JOINT_STIFFNESS }
+};
+
+// given a list of keywords that map strings to integer values denoting keyword types, this method will determine the string corresponding to the token passed in
+char* getKeyword(int lineType, KeyWord* keywords, int nKeywords) {
+	for (int i = 0; i<nKeywords; i++) {
+		if (lineType == keywords[i].retVal)
+			return keywords[i].keyWord;
+	}
+	return NULL;
+}
+
+// determine the type of a line that was used in the input file for a rigid body
+int getRBLineType(char* &buffer) {
+	return getLineType(buffer, RBkeywords, sizeof(RBkeywords) / sizeof(RBkeywords[0]));
+}
+
+// returns the string associated with the given token
+char* getRBString(int token) {
+	return getKeyword(token, RBkeywords, sizeof(RBkeywords) / sizeof(RBkeywords[0]));
+}
--- src/T.cpp
+++ src/T.cpp
@@ -1,5 +1,7 @@
 
+#include "..\include\Utils.h"
 #include "..\include\T.h"
+
 
 
 
@@ -17,12 +19,12 @@ void T::loadFromFile(FILE* fp) {
 	//this is where it happens.
 	while (!feof(fp)) {
 		//get a line from the file...
-		/*readValidLine(buffer, 200, fp);
+		readValidLine(buffer, 200, fp);
 		char *line = lTrim(buffer);
 		int lineType = getRBLineType(line);
 		switch (lineType) {
 		case RB_NAME:
-			this->name = std::string() + trim(line);
+			//this->name = std::string() + trim(line);
 			break;
 		case RB_MESH_NAME: {
 			char tmpStr[200];
@@ -35,134 +37,39 @@ void T::loadFromFile(FILE* fp) {
 			//meshes.push_back(tmpMesh);
 			//}		
 		}
-						   break;
-		case RB_MATERIAL:
-			if (meshes.size() > 0) {
-				char tmpStr[200];
-				sscanf(line, "%s", tmpStr);
-				GLShaderMaterial* shaderMaterial = GLContentManager::getShaderMaterial(tmpStr);
-				if (shaderMaterial == NULL)
-					Logger::consolePrint("RigidBody::loadFromFile: specified material not found...\n");
-				else
-					meshes[meshes.size() - 1]->setMaterial(*shaderMaterial);
-			}
-			else
-				Logger::consolePrint("RigidBody::loadFromFile: Warning, specified material does not apply to any mesh\n");
-			break;
-		case RB_COLOR: {
-			double r, g, b, a;
-			if (sscanf(line, "%lf %lf %lf %lf", &r, &g, &b, &a) != 4)
-				throwError("Incorrect rigid body input file - colour parameter expects 4 arguments (colour %s)\n", line);
-			if (meshes.size() > 0)
-				meshes[meshes.size() - 1]->getMaterial().setColor(r, g, b, a);
-		}
-					   break;
-		case RB_MASS: {
-			double t = 1;
-			if (sscanf(line, "%lf", &t) != 1)
-				Logger::consolePrint("Incorrect rigid body input file - a mass needs to be specified if the 'mass' keyword is used.");
-			this->rbProperties.mass = t;
-		}
-					  break;
-		case RB_MOI: {
-			double t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
-			sscanf(line, "%lf %lf %lf %lf %lf %lf", &t1, &t2, &t3, &t4, &t5, &t6);
-			if (t1 <= 0 || t2 <= 0 || t3 <= 0)
-				Logger::consolePrint("Incorrect values for the principal moments of inertia.");
-			this->rbProperties.setMOI(t1, t2, t3, t4, t5, t6);
-		}
-					 break;
-		case RB_POSITION:
-			if (sscanf(line, "%lf %lf %lf", &state.position[0], &state.position[1], &state.position[2]) != 3)
-				throwError("Incorrect rigid body input file - 3 arguments are required to specify the world coordinates position of a rigid body\n", line);
-			break;
-		case RB_ORIENTATION: {
-			double t = 0, t1 = 0, t2 = 0, t3 = 0;
-			if (sscanf(line, "%lf %lf %lf %lf", &t, &t1, &t2, &t3) != 4)
-				throwError("Incorrect rigid body input file - 4 arguments are required to specify the world coordinates orientation of a rigid body\n", line);
-			state.orientation.setRotationFrom(t, V3D(t1, t2, t3).toUnit());
-		}
-							 break;
-		case RB_VELOCITY:
-			if (sscanf(line, "%lf %lf %lf", &state.velocity[0], &state.velocity[1], &state.velocity[2]) != 3)
-				throwError("Incorrect rigid body input file - 3 arguments are required to specify the world coordinates velocity of a rigid body\n", line);
-			break;
-		case RB_ANGULAR_VELOCITY:
-			if (sscanf(line, "%lf %lf %lf", &state.angularVelocity[0], &state.angularVelocity[1], &state.angularVelocity[2]) != 3)
-				throwError("Incorrect rigid body input file - 3 arguments are required to specify the world coordinates angular velocity of a rigid body\n", line);
-			break;
-		case RB_FRICTION_COEFF:
-			if (sscanf(line, "%lf", &rbProperties.frictionCoeff) != 1)
-				throwError("Incorrect rigid body input file - Expecting a value for the friction coefficient");
-			if (rbProperties.frictionCoeff<0)
-				throwError("Incorrect rigid body input file - Friction coefficient should be >= 0");
-			break;
-		case RB_RESTITUTION_COEFF:
-			if (sscanf(line, "%lf", &rbProperties.restitutionCoeff) != 1)
-				throwError("Incorrect rigid body input file - Expecting a value for the restitution coefficient");
-			if (rbProperties.restitutionCoeff<0 || rbProperties.restitutionCoeff>1)
-				throwError("Incorrect rigid body input file - restitution coefficient should be between 0 and 1");
-			break;
-		case RB_END_EFFECTOR: {
-			RBEndEffector tmpEE(P3D(), 0.01);
-			sscanf(line, "%lf %lf %lf %lf %d %lf %lf %lf %d", &tmpEE.coords[0], &tmpEE.coords[1], &tmpEE.coords[2], &tmpEE.featureSize, &tmpEE.eeType, &tmpEE.localCoordsWheelAxis[0], &tmpEE.localCoordsWheelAxis[1], &tmpEE.localCoordsWheelAxis[2], &tmpEE.meshIndex);
-			rbProperties.endEffectorPoints.push_back(tmpEE);
-		}
-							  break;
-		case RB_BODY_POINT_FEATURE: {
-			P3D p;
-			double featureSize = 0.02;
-			sscanf(line, "%lf %lf %lf %lf", &p[0], &p[1], &p[2], &featureSize);
-			//					if (sscanf(line, "%lf %lf %lf", &p[0], &p[1], &p[2], &featureSize) != 3)
-			//						throwError("Incorrect rigid body input file - 3 arguments are required to specify a body point feature\n", line);
-			rbProperties.bodyPointFeatures.push_back(RBFeaturePoint(p, featureSize));
-		}
-									break;
-		case RB_NOT_IMPORTANT:
-			if (strlen(line) != 0 && line[0] != '#')
-				Logger::consolePrint("Ignoring input line: \'%s\'\n", line);
-			break;
-		case RB_CDP: {
-			CollisionDetectionPrimitive* newCDP = CollisionDetectionPrimitive::getCDPFromDefinition(lTrim(line));
-			if (newCDP == NULL)
-				Logger::consolePrint("Could not load CDP from definition string: %s\n", line);
-			else
-				cdps.push_back(newCDP);
-		}
-					 break;
-		case RB_IS_FROZEN:
-			this->rbProperties.isFrozen = true;
-			break;
-		case RB_END_RB:
-			if (meshDescriptions.size() != meshes.size())
-				meshDescriptions.resize(meshes.size());
-			return;//and... done
-			break;
-		case RB_THICKNESSS:
-			if (sscanf(line, "%lf", &rbProperties.thickness) != 1)
-				throwError("Incorrect rigid body input file - Expecting a value for the thickness coefficient");
-			break;
-		case RB_MESH_DESCRIPTION: {
-			char tmpStr[200];
-			sscanf(line, "%s", tmpStr);
-			std::string str(tmpStr);
-			meshDescriptions.push_back(str);
-		}
-								  break;
+		break;
 		case RB_MESH_TRANSFORMATION: {
-			Quaternion q;
+			/*Quaternion q;
 			V3D T;
 			sscanf(line, "%lf %lf %lf %lf %lf %lf %lf",
-				&q[0], &q[1], &q[2], &q[3], &T[0], &T[1], &T[2]);
-			meshTransformations.push_back(Transformation(q.getRotationMatrix(), T));
+			&q[0], &q[1], &q[2], &q[3], &T[0], &T[1], &T[2]);
+			meshTransformations.push_back(Transformation(q.getRotationMatrix(), T));*/
 			break;
 		}
+		case RB_MATERIAL:			
+		case RB_COLOR: 
+		case RB_MASS: 
+		case RB_MOI: 
+		case RB_POSITION:
+		case RB_ORIENTATION: 
+		case RB_VELOCITY:
+		case RB_ANGULAR_VELOCITY:
+		case RB_FRICTION_COEFF:
+		case RB_RESTITUTION_COEFF:
+		case RB_END_EFFECTOR: 
+		case RB_BODY_POINT_FEATURE: 
+		case RB_NOT_IMPORTANT:
+		case RB_CDP: 
+		case RB_IS_FROZEN:
+		case RB_END_RB:
+		case RB_THICKNESSS:
+		case RB_MESH_DESCRIPTION:
 		case RB_MAPPING_INFO:
-			sscanf(line, "%d %d", &mappingInfo.index1, &mappingInfo.index2);
+			throw("Incorrect rigid body input file: \'%s\' - unexpected line.", buffer);
 			break;
 		default:
-			throwError("Incorrect rigid body input file: \'%s\' - unexpected line.", buffer);
-		}*/
+			throw("Incorrect rigid body input file: \'%s\' - unexpected line.", buffer);
+		}
 	}
 	throw("Incorrect articulated body input file! No /End found");
 	
