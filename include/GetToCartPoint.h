f1609be609f121a5536ea65e043cdbdbba5b2c21
Matvey <matvey@mazorrobotics.com>
2019-04-04 22:44:01 +0300

    Collision objective + Compute Value

--- include/GetToCartPoint.h
+++ include/GetToCartPoint.h
@@ -21,3 +21,19 @@ public:
 	virtual void addGradientTo(dVector& grad, const dVector& p);
 };
 
+class CollisionObjective : public ObjectiveFunction
+{
+	int m_numOfJoints;
+	P3D m_point;
+	double m_radius;
+	Robot* m_robot;
+	SingleArmKinematicsSolver kSolver;
+public:
+	CollisionObjective(int numOfJoints, double weight, P3D point, double radius, Robot* robot);
+	~CollisionObjective();
+
+	virtual double computeValue(const dVector& p);
+	virtual void addHessianEntriesTo(DynamicArray<MTriplet>& hessianEntries, const dVector& p);
+	virtual void addGradientTo(dVector& grad, const dVector& p);
+};
+
--- src/GetToCartPoint.cpp
+++ src/GetToCartPoint.cpp
@@ -63,3 +63,67 @@ void GetToCartPoint::addGradientTo(dVector & grad, const dVector & p)
 		}
 	}
 }
+
+
+CollisionObjective::CollisionObjective(int numOfJoints, double weight, P3D point, double radius, Robot* robot) :
+	kSolver(robot),
+	m_numOfJoints(numOfJoints),
+	m_point(point),
+	m_radius(radius),
+	m_robot(robot)
+{
+	this->weight = weight;
+}
+
+CollisionObjective::~CollisionObjective()
+{
+}
+
+double CollisionObjective::computeValue(const dVector & p)
+{
+	double result = 0;
+	int numOfPoints = p.rows() / m_numOfJoints;
+	for (size_t i = 0; i < numOfPoints; i++)
+	{
+		VectorXd q = p.segment(m_numOfJoints*i, m_numOfJoints);
+		P3D cart_pos = kSolver.CalcForwardKinematics(q);
+		double d = (cart_pos - m_point).norm();
+		result += d > m_radius ? 0 : (d - m_radius)*(d - m_radius);
+	}
+	return result;
+}
+
+
+void CollisionObjective::addHessianEntriesTo(DynamicArray<MTriplet>& hessianEntries, const dVector & p)
+{
+
+}
+
+void CollisionObjective::addGradientTo(dVector & grad, const dVector & p)
+{
+	int numOfPoints = p.rows() / m_numOfJoints;
+	for (int i = 0; i < numOfPoints; ++i)
+	{
+		MatrixNxM J;
+		VectorXd q = p.segment(m_numOfJoints*i, m_numOfJoints);
+		kSolver.compute_dpdq(q, J);
+
+		MatrixNxM Ja(3, 7);
+		Ja.col(0) = J.col(6);
+		Ja.col(1) = J.col(8);
+		Ja.col(2) = J.col(10);
+		Ja.col(3) = J.col(12);
+		Ja.col(4) = J.col(14);
+		Ja.col(5) = J.col(16);
+		Ja.col(6) = J.col(18);
+
+		P3D cart_pos = kSolver.CalcForwardKinematics(q);
+
+		MatrixNxM res(1, 7);
+		res = (cart_pos - m_point).transpose()*Ja;
+		for (size_t j = 0; j < m_numOfJoints; j++)
+		{
+			grad(i * m_numOfJoints + j) += res(j);
+		}
+	}
+}
