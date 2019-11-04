ca133a73bf44603e9cb600d513cc652e3f95fbe0
Matvey <matvey.rzhavskiy@medtronic.com>
2019-11-04 19:26:05 +0200

    CorrectPointToRobotEnvelope2D

--- Python scripts/2joints solution.py
+++ Python scripts/2joints solution.py
@@ -138,6 +138,28 @@ def CalcInverseKinematics2J3D(links,w,target):
     return np.array([[teta1_1 % (2*np.pi) ,teta1_2 % (2*np.pi)],
                      [teta2_1 % (2*np.pi) ,teta2_2 % (2*np.pi)]])
 
+def CorrectPointToRobotEnvelope2D(links,target):
+    """    
+    Returns nearest point to 'target' point, robot can reach
+    """
+    l1_length = np.linalg.norm(links[:,0])
+    l2_length = np.linalg.norm(links[:,1])
+    target_length = np.linalg.norm(target)
+    inner_radius = np.abs(l1_length-l2_length)
+    outer_radius = l1_length + l2_length
+    
+    if target_length <= outer_radius and target_length >= inner_radius:
+        return target
+    dir_vec = normalize(target)
+    if target_length > outer_radius:
+        return outer_radius * dir_vec
+    elif target_length < inner_radius:
+        return inner_radius * dir_vec
+    else:
+        raise Exception("Shouldn't happen")
+        
+    
+
 l_3d = np.array([[5,0],
                  [0,6],
                  [0,0]])
@@ -148,7 +170,7 @@ w = np.array([[0,1],
 
 theta = np.array([np.deg2rad(0),np.deg2rad(0)])
 #point_3d = FK_2(l_3d,w,theta)
-point_3d = [5,6,10]
+point_3d = [5,6,0]
 print(point_3d)
 [[t11,t12],[t21,t22]] = CalcInverseKinematics2J3D(l_3d,w,point_3d)
 target_for_verification1 = FK_2(l_3d,w,np.array([t11,t12]))
@@ -156,7 +178,30 @@ target_for_verification2 = FK_2(l_3d,w,np.array([t21,t22]))
 print(np.rad2deg([t11,t12]),target_for_verification1)
 print(np.rad2deg([t21,t22]),target_for_verification2)
 print (compare(target_for_verification1,target_for_verification2))
+#%% New semester 2020A. First meeting with Roi
+print (CorrectPointToRobotEnvelope2D(l_3d,np.array([-0.5,0,0])))
 #%%
+n = np.array([2,2,0])
+x = np.array([6,0,0]) + 3 * n
+print (x)
+#%%
+
+r = 90
+d = 360/r
+rot_mat = CreateRotationMatrix(np.deg2rad(d),np.array([0,0,1]))
+p = np.array([3,0,0])
+for step_1 in range(r):
+    p = np.dot(rot_mat,p)
+    l1.append(p[0])
+    l2.append(p[1])
+    #print (p)
+
+plt.figure(figsize=(10,10),dpi=100, facecolor='w', edgecolor='red')
+plt.scatter(l1,l2)
+plt.show()
+
+
+#%% Testing all permutations of thetas
 ran = 360
 for t_1 in range(ran):
     for t_2 in range(ran): 
@@ -184,10 +229,7 @@ point_2d = np.array([5,5])
 t1,t2 = CalcInverseKinematics2J2D(l_2d,point_2d)
 print (np.rad2deg(t1),np.rad2deg(t2))
 
-#%%  Test GetAngleBetweenVec function
-
-
-    
+#%%  Test GetAngleBetweenVec function    
 x = np.array([1,1,0])
 y = np.array([1,0,0])
 z = np.array([0,0,1])
@@ -223,3 +265,33 @@ t4 = np.array([np.deg2rad(100.38885782),np.deg2rad(180)])
 p = FK_2(l_3d,w,t4)
 print(p)
 
+#%% Plot example
+import numpy as np
+import matplotlib.pyplot as plt
+np.set_printoptions(precision=1,suppress=True)
+
+l_3d_test = np.array([[5,20],
+                      [0,0],
+                      [0,0]])
+
+w_test = np.array([[0,0],
+                   [0,0],
+                   [1,1]])
+
+l1 = []
+l2 = []
+
+ran = 360
+step = 9
+for t_1 in range(0,ran,step):
+    for t_2 in range(0,ran,step): 
+        #print(t_1,t_2)
+        theta = np.array([np.deg2rad(t_1),np.deg2rad(t_2)])
+        #input(theta)
+        point_3d = FK_2(l_3d_test,w_test,theta)
+        l1.append(point_3d[0])
+        l2.append(point_3d[1])
+
+plt.figure(figsize=(10,10),dpi=100, facecolor='w', edgecolor='red')
+plt.scatter(l1,l2)
+plt.show()
