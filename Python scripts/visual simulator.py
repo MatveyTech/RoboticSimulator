866404e92b286f69f8ba1a0ae8f5bb29f894f25c
Matvey <matvey.rzhavskiy@medtronic.com>
2020-03-28 21:51:43 +0300

    Simulation

--- Python scripts/visual simulator.py
+++ Python scripts/visual simulator.py
@@ -13,6 +13,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 GRAY = (240,240,240)
 SLATEGREY = (112,128,144)
+HALF_SLATEGREY = (184, 192, 200)
 WHITESMOKE = (245,245,245)
 JOINTSCOLOR = (32,178,170)
 
@@ -71,7 +72,7 @@ class Robot:
 #        self.j_axes = w
 #        self.tetas = tetas
         
-    def __init__(self):
+    def __init__(self,isSimulation=False):
         
         self.links = np.array([[200, 150, 250],
                        [0,0,0],
@@ -85,7 +86,10 @@ class Robot:
         rad10 = np.deg2rad(10)
         #self.tetas = np.array([rad10,rad10,rad10],dtype=float)
         self.tetas = np.array([0.4102, 5.7474, 1.005])
-        
+        self.linksColor = SLATEGREY if not isSimulation else HALF_SLATEGREY
+        self.linksWidth = 15 if not isSimulation else 8
+        self.jointsRad =  15 if not isSimulation else 8
+        self.eeRad =  23 if not isSimulation else 14
         
     def Draw(self,ws):
         fk_all = FK_ALL(self.links,self.j_axes,self.tetas)
@@ -94,7 +98,7 @@ class Robot:
         for i in range(len(fk_ext)-1): 
             p_from = TransformToScreen((fk_ext[ind][0],fk_ext[ind][1]))
             p_to   = TransformToScreen((fk_ext[ind+1][0],fk_ext[ind+1][1]))
-            pygame.draw.line(ws,SLATEGREY,p_from,p_to,15)
+            pygame.draw.line(ws,self.linksColor,p_from,p_to,self.linksWidth)
             ind = ind+1
             
         ind = 0
@@ -102,20 +106,88 @@ class Robot:
         for i in range(len(fk_ext)-1): 
             p_from = TransformToScreen((fk_ext[ind][0],fk_ext[ind][1]))
             p_to   = TransformToScreen((fk_ext[ind+1][0],fk_ext[ind+1][1]))
-            pygame.draw.circle(ws, BLACK, p_from, 15, 0)
+            pygame.draw.circle(ws, BLACK, p_from, self.jointsRad, 0)
             ind = ind+1
-        pygame.draw.circle(ws, BLACK, p_to, 23, 0)   
+        pygame.draw.circle(ws, BLACK, p_to, self.eeRad, 0)   
         
     def Move(self,tetas):
         self.tetas = tetas
         
     def IsCloseToTarget(self,target):
-        threshold = 2
+        threshold = 1
         
         fk = FK(self.links,self.j_axes,self.tetas)   
         delta = fk - target
         dist = np.sqrt(delta[0]*delta[0]+delta[1]*delta[1]+delta[2]*delta[2])
         return dist < threshold
+
+
+class Variables:
+    def __init__(self, nj,npts,data): 
+        
+        self.nJ = nj
+        self.nP = npts
+        self.nE = 3
+        self.bS = nj + self.nE #blockSize
+        if data.shape[0] != (self.nJ+self.nE)*self.nP :
+            raise "Variables class: Can't initialize variables class. Bad data dimentions!!!"
+        #self.data = np.zeros((self.nJ+self.nE)*self.nP)
+        self.data = data
+        
+    def GetTheta(self,i):
+        blockInd = i*self.bS
+        return self.data[blockInd:blockInd+self.nJ]
+    
+    def SetTheta(self,i,t):
+        if t.shape[0] != self.nJ :
+            raise "Variables class: Bad Tetha dimentions!!!"
+        blockInd = i*self.bS
+        self.data[blockInd:blockInd+self.nJ] = t
+    
+    def GetEE(self,i):        
+        blockInd = i*self.bS
+        return self.data[blockInd+self.nJ:blockInd+self.nJ+self.nE]
+    
+    def SetEE(self,i,ee):
+        if ee.shape[0] != self.nE :
+            raise "Variables class: Bad EE dimentions!!!"
+        blockInd = i*self.bS
+        self.data[blockInd+self.nJ:blockInd+self.nJ+self.nE] = ee
+        
+        
+class Simulation:
+    def __init__(self,npts):
+        self.nJ = 3
+        self.nP = npts
+        self.Robots = np.ndarray((npts,),dtype=np.object)
+        #self.var = Variables(self.nJ,self.nP)
+
+        for i in range(npts):
+            self.Robots[i] = Robot(True)
+    
+    
+    def Move(self,var):
+        v = Variables(self.nJ,self.nP,var)
+        for i in range(self.nP):
+            self.Robots[i].Move(v.GetTheta(i))
+        
+    
+    def Draw(self,windowSurface):
+        for i in range(self.nP):
+            self.Robots[i].Draw(windowSurface)
+        
+#sim = Simulation(10)
+
+
+
+#y = Variables(2,2)
+#print (y.GetTheta(0))
+#y.SetEE(0,np.array([100,200,300]))
+#print (y.GetTheta(0))
+#print (y.data)
+#
+#print (y.GetEE(1))
+        
         
 def DrawGrid(w,rows,surface):
     sizeBtwn = w // rows
@@ -147,7 +219,20 @@ def DrawMouseCoordinates(m,s):
     stri = "({0},{1})".format(mt[0],mt[1])
     pygame.display.set_caption(stri)
     
+
+def EqualPath(start,end,nP):
+    jn = start.shape[0]    
+    blockSize = jn + 3
+    step_arr = (end-start) / nP
+    res  = np.zeros(nP*(jn+3))
+    res[0:jn] = start
+    lastBlockInd = blockSize*(nP - 1)
+    res[lastBlockInd:lastBlockInd+jn] = end
+    for i in range(1,nP-1): 
+        res[i*blockSize:i*blockSize+jn] = start + i * step_arr
+    return res
     
+ 
 # set up pygame
 pygame.init()
 font = pygame.font.Font('freesansbold.ttf', 32)
@@ -157,17 +242,23 @@ screenSize = (screenW, screenH)
 windowSurface = pygame.display.set_mode(screenSize, 0, 32)
 pygame.display.set_caption('Thesis')
 
-tgt_st = DraggableCircle(BLUE,( 250,450),30,screenSize)
+tgt_st = DraggableCircle(BLUE,( 445,145),30,screenSize)
 tgt_en   = DraggableCircle(BLUE,(-250,450),30,screenSize)
 
 draggingObject = None
 
+State = AppState.Rest
 
-
-robot = Robot()
+robot_start = Robot()
+robot_final = Robot()
+num_Of_Points = 10
+sim = Simulation(num_Of_Points)
 #tm = ThesisMinimizer(robot.links,robot.j_axes,robot.tetas,np.array([ds1_position[0],ds1_position[1],0]))
-tm = ThesisMinimizer(robot.links,robot.j_axes,robot.tetas,np.array([600,0,0]))
+target = np.array([600,0,0])
+tm_start = ThesisMinimizer(robot_start.links,robot_start.j_axes,robot_start.tetas,target)
+tm_final = ThesisMinimizer(robot_final.links,robot_final.j_axes,robot_final.tetas,-target)
 
+IsCalculatingPathNow = False
 
 # run the game loop
 while True:
@@ -176,22 +267,46 @@ while True:
     DrawFrame(windowSurface)
     mouse = pygame.mouse.get_pos()
     
-    DrawMouseCoordinates(mouse,windowSurface) 
-     
-    if draggingObject != None:
+    DrawMouseCoordinates(mouse,windowSurface)     
+    
+        
+    if draggingObject == tgt_st:
         draggingObject.Move(mouse)
-        tm.UpdateTarget(np.array([draggingObject.position[0],draggingObject.position[1],0]))
+        tm_start.UpdateTarget(np.array([draggingObject.position[0],draggingObject.position[1],0]))
+        
+    if draggingObject == tgt_en:
+        draggingObject.Move(mouse)
+        tm_final.UpdateTarget(np.array([draggingObject.position[0],draggingObject.position[1],0]))
     
     tgt_st.Draw(windowSurface,mouse)
     tgt_en.Draw(windowSurface,mouse)
     
-    if not robot.IsCloseToTarget(tm.tgt):
-        print ("Making step")
-        tm.MakeStep()
-        robot.Move(tm.theta)
-    robot.Draw(windowSurface)
+    if  IsCalculatingPathNow:
+        v = EqualPath(robot_start.tetas,robot_final.tetas,num_Of_Points)
+        sim.Move(v)
+        #print (np.rad2deg(v))
+        #break
+        sim.Draw(windowSurface)
     
+    if not robot_start.IsCloseToTarget(tm_start.tgt):
+        print ("Making step start")
+        tm_start.MakeStep()
+        robot_start.Move(tm_start.theta)
+    robot_start.Draw(windowSurface)
+    
+    if not robot_final.IsCloseToTarget(tm_final.tgt):
+        print ("Making final start")
+        tm_final.MakeStep()
+        robot_final.Move(tm_final.theta)
+    robot_final.Draw(windowSurface)
+    
+    
+
+        
     pygame.display.update()
+    
+#    b = ReadBoolFromIni("Autostep")
+#    print(b)
     
     for event in pygame.event.get():        
         if event.type == QUIT:
@@ -205,3 +320,20 @@ while True:
                 draggingObject = tgt_en
         elif event.type == pygame.MOUSEBUTTONUP:
             draggingObject = None
+        elif event.type == pygame.KEYDOWN:
+            IsCalculatingPathNow = not IsCalculatingPathNow
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
