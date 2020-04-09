bee23553c1c3ec993cc7acd8ed7b9afa97703a8f
Matvey <matvey.rzhavskiy@medtronic.com>
2020-04-09 13:14:28 +0300

    SmoothnessObj ConputeVal and AddGradientTo

--- Python scripts/Objectives.py
+++ Python scripts/Objectives.py
@@ -32,14 +32,27 @@ class CompitabilityObj:
     
 
 class SmoothnessObj:
-    def __init__(self):
-        self.x = 5
+    def __init__(self,nj,npoints,w=1):
+        self.weight = w
+        ne = 3
+        blockSize = ne+nj
+        c = blockSize * npoints
+        r = c - blockSize - nj
+        self.A = np.zeros((r,c))
+        for i in range(0,r):
+            self.A[i,i+nj]=-1
+            self.A[i,i+nj+blockSize]=1        
+        self.ATA = np.dot(np.transpose(self.A),self.A)
         
-    def ComputeValue(self,p):
-        pass
+    def ComputeValue(self,curr):
+        m = np.dot(self.A,curr)
+        return norm_2(m) * self.weight
+        
     
-    def ComputeGradient(self,p):
-        pass
+    def AddGradientTo(self,curr,grad):
+        tres = 2 * self.weight * np.dot(self.ATA,curr)
+        grad += tres
+        
     
     def ComputeHessian(self,p):
         pass
@@ -74,17 +87,17 @@ class FinalObj:
         self.ee_final = ee_final
         self.weight = w
         
-    def ComputeValue(self,var):
-        lastPos = var.GetEE(var.LastIndex)
+    def ComputeValue(self,curr):
+        lastPos = curr.GetEE(var.LastIndex)
         if lastPos.shape[0] != 3 :
             raise "FinalObj is working now only with the EE dimention of 3!"
         diff = lastPos - self.ee_final
         return (norm_2(diff)) * self.weight
     
     def AddGradientTo(self,curr,grad):
-        lastPos = curr.GetEE(var.LastIndex)
+        lastPos = curr.GetEE(curr.LastIndex)
         grad_val = 2 *(lastPos - self.ee_final)*self.weight
-        grad.AddEE(var.LastIndex, grad_val)        
+        grad.AddEE(curr.LastIndex, grad_val)        
     
     def AddHessianTo(self,curr,hess):
         fr = curr.GetLastEEInd()
@@ -109,9 +122,14 @@ class FinalObj:
 so = FinalObj(np.array([0,0,0]))
 v = Variables(3,10)
 hess = np.zeros((60,60))
-print(v)
+#print(v)
 so.AddHessianTo(v,hess)
-print(hess)
+#print(hess)
+
+v.SetEE(0,np.array([5,5,5]))
+v.SetEE(2,np.array([1,2,3]))
+sm_o = SmoothnessObj(3,10)
+#print(sm_o.ComputeValue(v))
 
 #from tkinter import *
 #
