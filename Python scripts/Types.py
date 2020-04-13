3df8fc7019761ee435cef95be3458001a1ae6c7f
Matvey <matvey.rzhavskiy@medtronic.com>
2020-04-13 17:02:04 +0300

    Variables - VerifyThetaInRange() function added

--- Python scripts/Types.py
+++ Python scripts/Types.py
@@ -7,6 +7,15 @@ Created on Sun Mar 29 00:02:36 2020
 import numpy as np
 
 class Variables:
+    
+    def VerifyThetaInRange(self):
+        f = lambda x: x % (2*np.pi)
+        for i in range(self.nP):
+            t = f(self.GetTheta(i))            
+            blockInd = i*self.bS
+            self.data[blockInd:blockInd+self.nJ] = t
+            
+            
     def __init__(self, nj,npts,data=None): 
         
         self.nJ = nj
@@ -20,29 +29,47 @@ class Variables:
             raise "Variables class: Can't initialize variables class. Bad data dimentions!!!"
         #self.data = np.zeros((self.nJ+self.nE)*self.nP)
         self.data = data
+        self.VerifyThetaInRange()        
+    
+     
+    def SetData(self, newData):
+        if newData.shape != self.data.shape:
+            raise "new data should be the same shape as the old one"
+        self.data = newData
+        self.VerifyThetaInRange()
         
+    
     def GetVal(self,i,j):
         return self.data[i*self.bS + j]
     
+    
     def SetVal(self,i,j,val):
         self.data[i*self.bS + j] = val
+        self.VerifyThetaInRange()
+    
     
     def AddVal(self,i,j,val):
         self.data[i*self.bS + j] = self.data[i*self.bS + j] + val
+        self.VerifyThetaInRange()
         
+    
     def GetTheta(self,i):
         blockInd = i*self.bS
         return self.data[blockInd:blockInd+self.nJ]
+    
     
     def SetTheta(self,i,t):
         if t.shape[0] != self.nJ :
             raise "Variables class: Bad Tetha dimentions!!!"
         blockInd = i*self.bS
         self.data[blockInd:blockInd+self.nJ] = t
+        self.VerifyThetaInRange()
+    
     
     def GetEE(self,i):        
         blockInd = i*self.bS
         return self.data[blockInd+self.nJ:blockInd+self.nJ+self.nE]
+    
     
     def SetEE(self,i,ee):
         if ee.shape[0] != self.nE :
@@ -50,6 +77,7 @@ class Variables:
         blockInd = i*self.bS
         self.data[blockInd+self.nJ:blockInd+self.nJ+self.nE] = ee
         
+    
     def AddEE(self,i,ee):
         if ee.shape[0] != self.nE :
             raise "Variables class: Bad EE dimentions!!!"
@@ -59,10 +87,19 @@ class Variables:
     def GetFirstEEInd(self):
         return self.nJ
     
+    
     def GetLastEEInd(self):
-        return self.bS*self.nP - self.nE
+        return self.bS*self.nP - self.nE    
     
     
     def __str__(self):
         x = np.reshape(self.data, (self.nP,self.bS))
         return x.__str__()
+#    
+#v = Variables(2,3,np.arange(15.0))
+#v.data[0]=6
+#v.data[5]=7.2
+#v.data[6]=-0.5
+#print(v)
+#v.VerifyThetaInRange()
+#print(v)
